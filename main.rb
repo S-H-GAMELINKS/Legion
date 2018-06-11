b8c7915fb3de247b41e6d71c04dcdfc9f25ba733
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-11 10:20:16 +0900

    parformence up

--- main.rb
+++ main.rb
@@ -21,20 +21,26 @@ i = 0
 
 Window.caption=("Legion is Mastodon Client")
 
+WAIT = 99999999999999
+
 Window.loop do
 
-    home_timeline = GetHomeTimeline(client)
-
-    for num in 0..10
-        #Window.draw_font(0, 30 * num, "#{home_timeline[num].account.username}", font)
-        Window.draw_font(0, 60 * num, "#{home_timeline[num].content}", font)
-    end
-
-    if Input.key_down?(K_RETURN) then
+    if Input.key_release?(K_RETURN) then
         message = Toot("")
         if message != "" then
             response = client.create_status(message.encode("UTF-8"))
         end
+    end
+    
+    if i % WAIT == 0 then
+        home_timeline = GetHomeTimeline(client)
+
+        for num in 0..10
+            #Window.draw_font(0, 30 * num, "#{home_timeline[num].account.username}", font)
+            Window.draw_font(0, 60 * num, "#{home_timeline[num].content}", font)
+        end
+    else
+        i += 1
     end
 
     #エスケープキーでループを抜ける
