5b1231835b6285757a3e4524b3c670dcee9968a4
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-11 10:34:40 +0900

    remove var 'x & y'

--- main.rb
+++ main.rb
@@ -5,7 +5,6 @@ require 'mastodon'
 require 'highline/import'
 require 'dotenv'
 require_relative 'input'
-require_relative 'mastodon'
 
 Dotenv.load
 
@@ -13,34 +12,31 @@ client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token:
 
 font = Font.new(18)
 
-x, y = 0, 0
-
 home_timeline = Hash.new
 
 i = 0
 
 Window.caption=("Legion is Mastodon Client")
 
-WAIT = 99999999999999
-
 Window.loop do
+
+    client.public_timeline.each do |toot|
+        home_timeline[i] = toot
+        i += 1
+    end
+
+    i = 0
+
+    for num in 0..10
+        #Window.draw_font(0, 30 * num, "#{home_timeline[num].account.username}", font)
+        Window.draw_font(0, 60 * num, "#{home_timeline[num].content}", font)
+    end
 
     if Input.key_release?(K_RETURN) then
         message = Toot("")
         if message != "" then
             response = client.create_status(message.encode("UTF-8"))
         end
-    end
-    
-    if i % WAIT == 0 then
-        home_timeline = GetHomeTimeline(client)
-
-        for num in 0..10
-            #Window.draw_font(0, 30 * num, "#{home_timeline[num].account.username}", font)
-            Window.draw_font(0, 60 * num, "#{home_timeline[num].content}", font)
-        end
-    else
-        i += 1
     end
 
     #エスケープキーでループを抜ける
