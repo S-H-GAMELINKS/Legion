81d9a4828be4fa1399d488423a8db7655710d269
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-11 11:00:54 +0900

    using MastodonAPI class

--- main.rb
+++ main.rb
@@ -5,32 +5,20 @@ require 'mastodon'
 require 'highline/import'
 require 'dotenv'
 require_relative 'input'
+require_relative 'mastodon'
 
 Dotenv.load
 
 client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_TOKEN"])
 
-font = Font.new(18)
-
-home_timeline = Hash.new
-
-i = 0
+mastodon = MastodonAPI.new(client)
 
 Window.caption=("Legion is Mastodon Client")
 
 Window.loop do
 
-    client.public_timeline.each do |toot|
-        home_timeline[i] = toot
-        i += 1
-    end
-
-    i = 0
-
-    for num in 0..10
-        #Window.draw_font(0, 30 * num, "#{home_timeline[num].account.username}", font)
-        Window.draw_font(0, 60 * num, "#{home_timeline[num].content}", font)
-    end
+    mastodon.GetHomeTimeline
+    mastodon.DrawHomeTimeline
 
     if Input.key_release?(K_RETURN) then
         message = Toot("")
--- mastodon.rb
+++ mastodon.rb
@@ -1,13 +1,24 @@
-def GetHomeTimeline(client)
+class MastodonAPI
 
-    home_timeline = Hash.new
-
-    i = 0
-
-    client.home_timeline.each do |toot|
-        home_timeline[i] = toot
-        i += 1
+    def initialize(client)
+        @client = client
+        @timeline = Hash.new
+        @font = Font.new(18)
     end
 
-    return home_timeline
+    def GetHomeTimeline
+        i = 0
+
+        @client.home_timeline.each do |toot|
+            @timeline[i] = toot
+            i += 1
+        end
+    end
+
+    def DrawHomeTimeline
+        for num in 0..5
+            #Window.draw_font(0, 30 * num, "#{home_timeline[num].account.username}", font)
+            Window.draw_font(0, 60 * num, "#{@timeline[num].content}", @font)
+        end
+    end
 end
