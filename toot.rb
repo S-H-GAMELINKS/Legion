5744e1840fc9dba0b3f58e54c6f2924ed150834e
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-13 16:32:35 +0900

    add home_timeline.rb

--- toot.rb
+++ toot.rb
@@ -17,9 +17,6 @@ Dotenv.load
 client = Mastodon::REST::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_TOKEN"])
 mastodon = MastodonAPI.new(client)
 
-stream = Mastodon::Streaming::Client.new(base_url: ENV["MASTODON_URL"], bearer_token: ENV["MASTODON_TOKEN"])
-streaming = MastodonStreaming.new(stream)
-
 var = TkVariable.new('')
 
 text = TkText.new(nil, 'width' => '30', 'height'=> '30')
@@ -36,9 +33,5 @@ mediabutton.pack('side' => 'left', 'fill' => 'both')
 quitbutton = TkButton.new(nil, 'text' => 'quit',
 		  'command' => proc{exit})
 quitbutton.pack('side' => 'right', 'fill' => 'both')
-
-timeline_window = TkToplevel.new
-timeline_window.title('ホームタイムライン')
-streaming.HomeTimeline(timeline_window)
 
 Tk.mainloop
