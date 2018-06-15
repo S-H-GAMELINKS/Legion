58909f8ae8e7c623b486982e488985c2740b85ae
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-15 16:45:34 +0900

    return ftl

--- mastodon.rb
+++ mastodon.rb
@@ -44,7 +44,7 @@ class MastodonStreaming
         @stream.firehose() do |toot|
             message = Nokogiri::HTML.parse(toot.content, nil, nil).search('p')
             ftl = TkMessage.new(window, 'text' => message.text)
-            ftl.pack('side' => 'bottom')
+            return ftl
         end
     end
 
