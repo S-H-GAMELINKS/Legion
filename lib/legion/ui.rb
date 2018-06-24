ff6308cfd8d784fc41802a3830ef76809e74086c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-24 11:17:40 +0900

    remove return

--- lib/legion/ui.rb
+++ lib/legion/ui.rb
@@ -11,25 +11,23 @@ class Timeline
 	end
 
 	def timeline_pack
-		return @timeline.pack('side' => 'left', 'fill' => 'both')
+		@timeline.pack('side' => 'left', 'fill' => 'both')
 	end
 
 	def label_pack
-		return @label.pack('side' => 'top', 'fill' => 'both')
+		@label.pack('side' => 'top', 'fill' => 'both')
 	end
 
 	def list_pack
-		return @list.pack('fill' => 'both')
+		@list.pack('fill' => 'both')
 	end
 
 	def y_scrollbar		
-		@list['yscrollcommand'] = proc{|*args| @timeline_yscrollbar.set(*args);}
-		return @timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
+		@timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
 	end
 
 	def x_scrollbar
-		@list['xscrollcommand'] = proc{|*args| @timeline_xscrollbar.set(*args);}
-		return @timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
+		@timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
     end
     
     def set
