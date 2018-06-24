a6a67953ddfa9ace311789c042cc7b154656076b
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-24 11:32:19 +0900

    fix set methods

--- lib/legion/ui.rb
+++ lib/legion/ui.rb
@@ -22,17 +22,23 @@ class Timeline
 		@list.pack('fill' => 'both')
 	end
 
-	def y_scrollbar		
+	def y_scrollbar_pack
+		@timeline_yscrollbar = TkScrollbar.new(@list) {orient "vertical"; command proc{|*args| @list.yview(*args);} }
+		@list['yscrollcommand'] = proc{|*args| @timeline_yscrollbar.set(*args);}
 		@timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
 	end
 
-	def x_scrollbar
+	def x_scrollbar_pack
+		@timeline_xscrollbar = TkScrollbar.new(@list) {orient "horizontal"; command proc{|*args| @list.xview(*args);} }
+		@list['xscrollcommand'] = proc{|*args| @timeline_xscrollbar.set(*args);}
 		@timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
     end
     
     def set
         self.timeline_pack
         self.label_pack
-        self.list_pack
+		self.list_pack
+		self.y_scrollbar_pack
+		self.x_scrollbar_pack
     end
 end
