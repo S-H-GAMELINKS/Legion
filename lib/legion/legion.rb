571c37231897e5d72efd5b550838131c11ab125a
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-06-24 12:51:48 +0900

    refix scrollbar

--- lib/legion/legion.rb
+++ lib/legion/legion.rb
@@ -25,11 +25,35 @@ tootFrame.set
 home_timeline = Timeline.new(window, "ホームタイムライン")
 home_timeline.set
 
+home_timeline_yscrollbar = TkScrollbar.new(home_timeline.list) {orient "vertical"; command proc{|*args| home_timeline.list.yview(*args);} }
+home_timeline.list['yscrollcommand'] = proc{|*args| home_timeline_yscrollbar.set(*args);}
+home_timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
+
+home_timeline_xscrollbar = TkScrollbar.new(home_timeline.list) {orient "horizontal"; command proc{|*args| home_timeline.list.xview(*args);} }
+home_timeline.list['xscrollcommand'] = proc{|*args| home_timeline_xscrollbar.set(*args);}
+home_timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
+
 local_timeline = Timeline.new(window, "ローカルタイムライン")
 local_timeline.set
 
+local_timeline_yscrollbar = TkScrollbar.new(local_timeline.list) {orient "vertical"; command proc{|*args| local_timeline.list.yview(*args);} }
+local_timeline.list['yscrollcommand'] = proc{|*args| local_timeline_yscrollbar.set(*args);}
+local_timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
+
+local_timeline_xscrollbar = TkScrollbar.new(local_timeline.list) {orient "horizontal"; command proc{|*args| local_timeline.list.xview(*args);} }
+local_timeline.list['xscrollcommand'] = proc{|*args| local_timeline_xscrollbar.set(*args);}
+local_timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
+
 public_timeline = Timeline.new(window, "連合タイムライン")
 public_timeline.set
+
+public_timeline_yscrollbar = TkScrollbar.new(public_timeline.list) {orient "vertical"; command proc{|*args| public_timeline.list.yview(*args);} }
+public_timeline.list['yscrollcommand'] = proc{|*args| public_timeline_yscrollbar.set(*args);}
+public_timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
+
+public_timeline_xscrollbar = TkScrollbar.new(public_timeline.list) {orient "horizontal"; command proc{|*args| public_timeline.list.xview(*args);} }
+public_timeline.list['xscrollcommand'] = proc{|*args| public_timeline_xscrollbar.set(*args);}
+public_timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
 
 loop do
 	Parallel.each([[1, home_timeline, home_timeline.list], [2, local_timeline, local_timeline.list], [3, public_timeline, public_timeline.list]], in_threads: 3) do |call|
--- lib/legion/ui.rb
+++ lib/legion/ui.rb
@@ -6,8 +6,6 @@ class Timeline
 		@timeline				= TkFrame.new(window)
 		@label					= TkLabel.new(@timeline, 'text' => timeline_name, 'width' => 50)
 		@list					= TkListbox.new(@timeline, 'height' => 25, 'selectmode' => 'multiple')
-		@timeline_xscrollbar	= TkScrollbar.new(@list) {orient "vertical"; command proc{|*args| @list.yview(*args);} }
-		@timeline_yscrollbar	= TkScrollbar.new(@list) {orient "horizontal"; command proc{|*args| @list.xview(*args);} }
 	end
 
 	def timeline_pack
@@ -21,25 +19,11 @@ class Timeline
 	def list_pack
 		@list.pack('fill' => 'both')
 	end
-
-	def y_scrollbar_pack
-		@timeline_yscrollbar = TkScrollbar.new(@list) {orient "vertical"; command proc{|*args| @list.yview(*args);} }
-		@list['yscrollcommand'] = proc{|*args| @timeline_yscrollbar.set(*args);}
-		@timeline_yscrollbar.pack('side' => 'right', 'fill' => 'both', 'ipadx' => '5', 'ipady' => '170')
-	end
-
-	def x_scrollbar_pack
-		@timeline_xscrollbar = TkScrollbar.new(@list) {orient "horizontal"; command proc{|*args| @list.xview(*args);} }
-		@list['xscrollcommand'] = proc{|*args| @timeline_xscrollbar.set(*args);}
-		@timeline_xscrollbar.pack('side' => 'bottom', 'fill' => 'both')
-    end
     
     def set
         self.timeline_pack
         self.label_pack
 		self.list_pack
-		self.y_scrollbar_pack
-		self.x_scrollbar_pack
     end
 end
 
